ff8779a8ac0d4990a0dea03e7b9d19767fa4d711
Shambarick <shambarick@users.noreply.github.com>
2021-02-17 14:29:17 -1000

    Add env_file support

--- 6/docker-entrypoint.sh
+++ 6/docker-entrypoint.sh
@@ -1,16 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [[ "${!var:-}" ]] && [[ "${!fileVar:-}" ]]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [[ "${!var:-}" ]]; then
+		val="${!var}"
+	elif [[ "${!fileVar:-}" ]]; then
+		val="$(< "${!fileVar}")"
+	fi
+    export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env REDIS_PASSWORD
+
+if [ ! -z "$REDIS_PASSWORD" ]; then
+    REDIS_AUTH="--requirepass ${REDIS_PASSWORD}"
+fi
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@"
+	set -- redis-server "$@" "$REDIS_AUTH"
 fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	find . \! -user redis -exec chown redis '{}' +
-	exec gosu redis "$0" "$@"
+	exec gosu redis "$0" "$@" "$REDIS_AUTH"
 fi
 
 exec "$@"
