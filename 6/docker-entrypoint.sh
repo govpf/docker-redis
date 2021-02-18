047c4b172f23935abe0d5af7c327501dfaf0a102
Shambarick <shambarick@users.noreply.github.com>
2021-02-17 14:57:31 -1000

    fix syntax

--- 6/docker-entrypoint.sh
+++ 6/docker-entrypoint.sh
@@ -26,19 +26,19 @@ file_env() {
 file_env REDIS_PASSWORD
 
 if [ ! -z "$REDIS_PASSWORD" ]; then
-    REDIS_AUTH="--requirepass ${REDIS_PASSWORD}"
+    REDIS_AUTH=" --requirepass ${REDIS_PASSWORD}"
 fi
 
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-	set -- redis-server "$@" "$REDIS_AUTH"
+	set -- redis-server "$@"
 fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	find . \! -user redis -exec chown redis '{}' +
-	exec gosu redis "$0" "$@" "$REDIS_AUTH"
+	exec gosu redis "$0" "$@"
 fi
 
-exec "$@"
+exec "${@}" ${REDIS_AUTH}
